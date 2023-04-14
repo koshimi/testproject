5a3e1a34e57f24e06ec06ebdc5dc1ee49e73373a
hk03-6 <hk03-6@DESKTOP-6LBPTVC>
2023-04-14 16:55:12 +0900

    main에 테스트 추가

--- demotest-2/src/main/java/com/example/demo/Demotest2Application.java
+++ demotest-2/src/main/java/com/example/demo/Demotest2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demotest2Application {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Git");
 		SpringApplication.run(Demotest2Application.class, args);
 	}
 
